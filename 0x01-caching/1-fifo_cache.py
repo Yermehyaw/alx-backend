51c393822f1f759c79b24936dab871bc9cbe8426
Yermehyaw <yermehyaw@gmail.com>
2024-10-30 06:01:07 +0000

    Fix: dont reove exiating key and amintain order of cache dict

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -49,15 +49,21 @@ class FIFOCache(BaseCaching):
         if not key or not item:  # if either is None
             return
 
+        # test key, item if they are already part of cache
+        value = self.cache_data.get(key)  # exceptions handled
+        if value:  # key exists
+            if value == item:
+                return
+            self.cache_data.update({key: item})  # item diffs
+            return
+
         if len(self.cache_index) == BaseCaching.MAX_ITEMS:
             first_added = self.cache_index[0]
-            if key in self.cache_index and key != first_added:
-                return
-
+            del self.cache_index[0]
             del self.cache_data[first_added]
             print(f'DISCARD: {first_added}')
 
-        self.cache_index.append(key)  ##
+        self.cache_index.append(key)
         self.cache_data.update({key: item})
 
     def get(self, key):
