d9d58c9d23fdd08fc39568d887480f9b9dfc9c59
Yermehyaw <yermehyaw@gmail.com>
2024-11-09 23:01:38 +0100

    Markup templates for translayion

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+A simple flask app
+
+Modules imported: flask
+
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+from flask_babel import (
+    Babel,
+    gettext,
+)
+
+
+class Config():
+    """Config for Babel obj"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get the locale lang defined from url"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Homepage"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
