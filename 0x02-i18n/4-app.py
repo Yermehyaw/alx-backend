ea08ad19550b0774ec0aceb1f50baa07b827e639
Yermehyaw <yermehyaw@gmail.com>
2024-11-09 23:02:50 +0000

    Enable forcong a spec locale from uri arg

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+A simple flask app
+
+Modules imported: flask, flask_babel
+
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+from flask_babel import (
+    Babel,
+    _,
+)
+
+
+class Config():
+    """Config for Babel obj"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get the locale lang defined from url"""
+    locale_arg = request.get.arg('locale')
+    if locale_arg in app.config['LANGUAGES']:
+        return locale_arg
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Homepage"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
