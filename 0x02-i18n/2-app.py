a53371ee5d429848a80425a05aa442fd9f5c1c9d
Yermehyaw <yermehyaw@gmail.com>
2024-11-09 21:39:12 +0000

    Fix typo

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -28,7 +28,7 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """Get the locale lang defined from url"""
-    return request.accept_language.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
