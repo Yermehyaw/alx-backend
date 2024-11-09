fab505c23dc04145809fc13d4902de21cc7881ca
Yermehyaw <yermehyaw@gmail.com>
2024-11-09 12:41:14 +0000

    A simple flask app with a template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+A simple flask app
+
+Modules imported: flask
+
+"""
+from flask import (
+    Flask,
+    render_template,
+)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Homepage"""
+    return render_template('0-index.html')
