3975c7a05e301dcc12bcba07db7534c5a33ce779
Yermehyaw <yermehyaw@gmail.com>
2024-11-10 12:44:40 +0000

    Adjust welcome message based on a valid user login status

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""
+A simple flask app
+
+Modules imported: flask, flask_babel, typing
+
+"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    Response,
+    g,
+)
+from flask_babel import (
+    Babel,
+    _,
+)
+from typing import (
+    Dict,
+    Union
+)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    """Config for Babel obj"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get the locale lang defined from url"""
+    locale_arg = request.args.get('locale')
+
+    if locale_arg in app.config['LANGUAGES']:
+        return locale_arg
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.before_request
+def before_request() -> None:
+    """Sets the user to the g global"""
+    # find user
+    user_info = get_user()
+
+    if user_info:
+        g.user = user_info
+
+
+@app.route('/')
+def index() -> Response:
+    """Homepage"""
+    return render_template('0-index.html')
+
+
+def get_user() -> Union[Dict, None]:
+    """Gets the info on a user by user id"""
+    user_id = request.args.get('login_as')
+
+    if user_id:
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            return
+
+        return users.get(user_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
