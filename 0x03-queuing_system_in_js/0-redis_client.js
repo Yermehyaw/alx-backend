18c202007098842cd82971b705e3190b24c53018
Yermehyaw <yermehyaw@gmail.com>
2024-12-04 18:33:15 -0800

    Fix typos

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -10,5 +10,3 @@ const client = createClient();
 client.on('error', err => console.log('Redis client not connected to the server: ', err));
 
 client.on('connect', () => console.log('Redis client connected to the server'));
-
-// await client.connect();
